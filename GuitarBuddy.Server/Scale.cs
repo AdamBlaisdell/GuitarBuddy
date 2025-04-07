ae4e6b48893dd81ed320e27590f80032eb792c35
AdamPC <adam.s.blaisdell@gmail.com>
2025-04-06 23:42:55 -0400

    Added Scale.cs

---
+++ GuitarBuddy.Server/Scale.cs
@@ -0,0 +1,9 @@
+﻿namespace GuitarBuddy.Server.Models
+{
+    public class Scale
+    {
+        public string Root { get; set; } = string.Empty;
+        public string Name { get; set; } = string.Empty; // e.g., "Major"
+        public List<string> Notes { get; set; } = new();
+    }
+}
